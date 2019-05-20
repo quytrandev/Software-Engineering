5a0fcd8edc06ab0d95816c4f1fcaef375df6b10e
Quy Tran <quytrandev@gmail.com>
2019-05-20 21:27:49 +0700

    Upload UI for software

---
+++ PetShop/PetShop/PetShop/frmLogin.Designer.cs
@@ -0,0 +1,285 @@
+﻿namespace PetShop
+{
+    partial class frmLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.cbShowPassword = new System.Windows.Forms.CheckBox();
+            this.btnSignIn = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.lblUsername = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.lblWelcome = new System.Windows.Forms.Label();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.lblVersion = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.panel1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Controls.Add(this.cbShowPassword);
+            this.panel1.Controls.Add(this.btnSignIn);
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Controls.Add(this.lblWelcome);
+            this.panel1.Location = new System.Drawing.Point(352, 36);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(652, 708);
+            this.panel1.TabIndex = 0;
+            // 
+            // cbShowPassword
+            // 
+            this.cbShowPassword.AutoSize = true;
+            this.cbShowPassword.BackColor = System.Drawing.Color.Transparent;
+            this.cbShowPassword.FlatAppearance.BorderSize = 0;
+            this.cbShowPassword.FlatAppearance.CheckedBackColor = System.Drawing.Color.Purple;
+            this.cbShowPassword.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Purple;
+            this.cbShowPassword.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Purple;
+            this.cbShowPassword.Font = new System.Drawing.Font("Arial", 13F, System.Drawing.FontStyle.Italic);
+            this.cbShowPassword.ForeColor = System.Drawing.Color.MediumBlue;
+            this.cbShowPassword.Location = new System.Drawing.Point(423, 418);
+            this.cbShowPassword.Margin = new System.Windows.Forms.Padding(4);
+            this.cbShowPassword.Name = "cbShowPassword";
+            this.cbShowPassword.Size = new System.Drawing.Size(186, 30);
+            this.cbShowPassword.TabIndex = 10;
+            this.cbShowPassword.Text = "Show password";
+            this.cbShowPassword.UseVisualStyleBackColor = false;
+            this.cbShowPassword.CheckedChanged += new System.EventHandler(this.cbShowPassword_CheckedChanged);
+            // 
+            // btnSignIn
+            // 
+            this.btnSignIn.BackColor = System.Drawing.Color.Black;
+            this.btnSignIn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnSignIn.FlatAppearance.BorderColor = System.Drawing.Color.MediumBlue;
+            this.btnSignIn.FlatAppearance.BorderSize = 0;
+            this.btnSignIn.FlatAppearance.MouseDownBackColor = System.Drawing.Color.MediumBlue;
+            this.btnSignIn.FlatAppearance.MouseOverBackColor = System.Drawing.Color.MediumBlue;
+            this.btnSignIn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSignIn.Font = new System.Drawing.Font("Arial", 30F, System.Drawing.FontStyle.Bold);
+            this.btnSignIn.ForeColor = System.Drawing.Color.White;
+            this.btnSignIn.Location = new System.Drawing.Point(115, 490);
+            this.btnSignIn.Margin = new System.Windows.Forms.Padding(4);
+            this.btnSignIn.Name = "btnSignIn";
+            this.btnSignIn.Size = new System.Drawing.Size(459, 71);
+            this.btnSignIn.TabIndex = 9;
+            this.btnSignIn.Text = "SIGN IN";
+            this.btnSignIn.UseVisualStyleBackColor = false;
+            this.btnSignIn.Click += new System.EventHandler(this.btnSignIn_Click);
+            this.btnSignIn.Enter += new System.EventHandler(this.btnSignIn_Enter);
+            this.btnSignIn.Leave += new System.EventHandler(this.btnSignIn_Leave);
+            this.btnSignIn.MouseEnter += new System.EventHandler(this.btnSignIn_MouseEnter);
+            this.btnSignIn.MouseLeave += new System.EventHandler(this.btnSignIn_MouseLeave);
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.lblUsername);
+            this.panel3.Controls.Add(this.pictureBox1);
+            this.panel3.Controls.Add(this.lblPassword);
+            this.panel3.Controls.Add(this.pictureBox2);
+            this.panel3.Controls.Add(this.txtPassword);
+            this.panel3.Controls.Add(this.txtUsername);
+            this.panel3.Location = new System.Drawing.Point(67, 150);
+            this.panel3.Margin = new System.Windows.Forms.Padding(4);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(564, 245);
+            this.panel3.TabIndex = 8;
+            // 
+            // lblUsername
+            // 
+            this.lblUsername.AutoSize = true;
+            this.lblUsername.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Bold);
+            this.lblUsername.Location = new System.Drawing.Point(41, 38);
+            this.lblUsername.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblUsername.Name = "lblUsername";
+            this.lblUsername.Size = new System.Drawing.Size(161, 35);
+            this.lblUsername.TabIndex = 4;
+            this.lblUsername.Text = "Username";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Image = global::PetShop.Properties.Resources.icons8_lock_50;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 140);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(41, 39);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Bold);
+            this.lblPassword.Location = new System.Drawing.Point(43, 144);
+            this.lblPassword.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(159, 35);
+            this.lblPassword.TabIndex = 6;
+            this.lblPassword.Text = "Password";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox2.Image = global::PetShop.Properties.Resources.icons8_customer_32;
+            this.pictureBox2.Location = new System.Drawing.Point(4, 38);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(41, 39);
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.txtPassword.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtPassword.Font = new System.Drawing.Font("Arial", 20F, System.Drawing.FontStyle.Italic);
+            this.txtPassword.ForeColor = System.Drawing.Color.LightGray;
+            this.txtPassword.Location = new System.Drawing.Point(48, 183);
+            this.txtPassword.Margin = new System.Windows.Forms.Padding(4);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(459, 39);
+            this.txtPassword.TabIndex = 7;
+            this.txtPassword.Text = "Type your password";
+            this.txtPassword.Click += new System.EventHandler(this.txtPassword_Click);
+            this.txtPassword.TextChanged += new System.EventHandler(this.txtPassword_TextChanged);
+            this.txtPassword.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtPassword_KeyPress);
+            this.txtPassword.Leave += new System.EventHandler(this.txtPassword_Leave);
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.txtUsername.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtUsername.Font = new System.Drawing.Font("Arial", 20F, System.Drawing.FontStyle.Italic);
+            this.txtUsername.ForeColor = System.Drawing.Color.LightGray;
+            this.txtUsername.Location = new System.Drawing.Point(49, 78);
+            this.txtUsername.Margin = new System.Windows.Forms.Padding(4);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(457, 39);
+            this.txtUsername.TabIndex = 3;
+            this.txtUsername.Text = "Type your username";
+            this.txtUsername.Click += new System.EventHandler(this.txtUsername_Click);
+            this.txtUsername.TextChanged += new System.EventHandler(this.txtUsername_TextChanged);
+            this.txtUsername.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtUsername_KeyPress);
+            this.txtUsername.Leave += new System.EventHandler(this.txtUsername_Leave);
+            // 
+            // lblWelcome
+            // 
+            this.lblWelcome.AutoSize = true;
+            this.lblWelcome.Font = new System.Drawing.Font("Arial Narrow", 50F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWelcome.ForeColor = System.Drawing.Color.MediumBlue;
+            this.lblWelcome.Location = new System.Drawing.Point(123, 52);
+            this.lblWelcome.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblWelcome.Name = "lblWelcome";
+            this.lblWelcome.Size = new System.Drawing.Size(402, 98);
+            this.lblWelcome.TabIndex = 0;
+            this.lblWelcome.Text = "WELCOME";
+            // 
+            // btnClose
+            // 
+            this.btnClose.BackColor = System.Drawing.Color.Transparent;
+            this.btnClose.FlatAppearance.BorderColor = System.Drawing.Color.LightGray;
+            this.btnClose.FlatAppearance.BorderSize = 0;
+            this.btnClose.FlatAppearance.MouseDownBackColor = System.Drawing.Color.LightGray;
+            this.btnClose.FlatAppearance.MouseOverBackColor = System.Drawing.Color.LightGray;
+            this.btnClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnClose.ForeColor = System.Drawing.Color.Transparent;
+            this.btnClose.Image = global::PetShop.Properties.Resources.close_24_white;
+            this.btnClose.Location = new System.Drawing.Point(1275, 0);
+            this.btnClose.Margin = new System.Windows.Forms.Padding(4);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(83, 33);
+            this.btnClose.TabIndex = 11;
+            this.btnClose.UseVisualStyleBackColor = false;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // lblVersion
+            // 
+            this.lblVersion.AutoSize = true;
+            this.lblVersion.BackColor = System.Drawing.Color.Transparent;
+            this.lblVersion.Font = new System.Drawing.Font("Arial", 10F);
+            this.lblVersion.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.lblVersion.Location = new System.Drawing.Point(1036, 763);
+            this.lblVersion.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblVersion.Name = "lblVersion";
+            this.lblVersion.Size = new System.Drawing.Size(0, 19);
+            this.lblVersion.TabIndex = 13;
+            // 
+            // frmLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MediumBlue;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1356, 786);
+            this.Controls.Add(this.lblVersion);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.panel1);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "frmLogin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblWelcome;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label lblUsername;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.CheckBox cbShowPassword;
+        private System.Windows.Forms.Button btnSignIn;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Label lblVersion;
+    }
+}
+
---
+++ PetShop/PetShop/PetShop/frmLogin.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PetShop
+{
+    public partial class frmLogin : Form
+    {
+        public frmLogin()
+        {
+            InitializeComponent();
+            //align panel giữa form
+            panel1.Location = new Point(
+            this.ClientSize.Width / 2 - panel1.Size.Width / 2,
+            this.ClientSize.Height / 2 - panel1.Size.Height / 2);
+            AutoLabel();
+            //mysql : 215 ; loc 803,620
+            //access: 213 width ; loc 803,620
+            //sql: 239 width, loc 777 620
+            //postgre: 246 width; loc 772 620
+        }
+        private void AutoLabel()
+        {
+            //if (version == "MySQL")
+            //{
+            //    lblVersion.Text = "Current database model: " + version;
+            //    lblVersion.Location = new Point(803, 620);
+            //}
+            //else if (version == "Access")
+            //{
+            //    lblVersion.Text = "Current database model: " + version;
+            //    lblVersion.Location = new Point(803, 620);
+
+            //}
+            //else if (version == "SQLServer")
+            //{
+            //    lblVersion.Text = "Current database model: " + version;
+            //    lblVersion.Location = new Point(777, 620);
+            //}
+            //else
+            //{
+            //    lblVersion.Text = "Current database model: " + version;
+            //    lblVersion.Location = new Point(772, 620);
+            //}
+        }
+        public static string UserName;
+        // ẩn/hiện password
+        private void ShowPassword()
+        {
+            if (cbShowPassword.Checked == true)
+                txtPassword.UseSystemPasswordChar = false;
+            else
+                txtPassword.UseSystemPasswordChar = true;
+        }
+
+        // xóa/đặt lại chú thích 'Type your username'
+        private void txtUsername_Click(object sender, EventArgs e)
+        {
+            if (txtUsername.Text == "Type your username")
+                txtUsername.ResetText();
+        }
+        private void txtUsername_Leave(object sender, EventArgs e)
+        {
+            if (txtUsername.Text == "")
+            {
+                txtUsername.Text = "Type your username";
+                txtUsername.ForeColor = System.Drawing.Color.LightGray;
+                txtUsername.Font = new Font(txtUsername.Font, FontStyle.Italic);
+            }
+
+        }
+        //
+
+        // xóa/đặt lại chú thích 'Type your password'
+        private void txtPassword_Click(object sender, EventArgs e)
+        {
+            ShowPassword();
+            if (txtPassword.Text == "Type your password")
+                txtPassword.ResetText();
+        }
+        private void txtPassword_Leave(object sender, EventArgs e)
+        {
+            if (txtPassword.Text == "")
+            {
+                txtPassword.Text = "Type your password";
+                txtPassword.ForeColor = System.Drawing.Color.LightGray;
+                txtPassword.Font = new Font(txtPassword.Font, FontStyle.Italic);
+                txtPassword.UseSystemPasswordChar = false;
+            }
+
+        }
+        //
+
+        //đổi fontstyle, màu chữ khi nhập username/password
+        private void txtUsername_TextChanged(object sender, EventArgs e)
+        {
+            txtUsername.Font = new Font(txtUsername.Font, FontStyle.Regular);
+            txtUsername.ForeColor = System.Drawing.Color.Black;
+
+        }
+        private void txtPassword_TextChanged(object sender, EventArgs e)
+        {
+
+            txtPassword.Font = new Font(txtPassword.Font, FontStyle.Regular);
+            txtPassword.ForeColor = System.Drawing.Color.Black;
+            txtPassword.UseSystemPasswordChar = true;
+        }
+        //
+
+
+        private void cbShowPassword_CheckedChanged(object sender, EventArgs e)
+        {
+            //hiện mật khẩu
+            ShowPassword();
+        }
+        private void txtUsername_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            //focus xuống textbox Mật khẩu
+            if (e.KeyChar == 13)
+            {
+                txtPassword.Focus();
+                ShowPassword();
+                txtPassword.ResetText();
+            }
+        }
+
+        private void txtPassword_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            //enter để đăng nhập
+            if (e.KeyChar == 13)
+            {
+                string username = txtUsername.Text;
+                string password = txtPassword.Text;
+
+                try
+                {
+                    //if (loginBL.CheckLogin(username, password))
+                    //{
+                    //    UserName = username;
+                    //    frmProducts frm = new frmProducts();
+                    //    this.Hide();
+                    //    frm.Show();
+                    //}
+                }
+                catch
+                {
+                    //Form frm = new frmWP();
+                    //frm.ShowDialog();
+                    this.btnSignIn.BackgroundImage = null;
+                    this.btnSignIn.BackColor = System.Drawing.Color.Black;
+                    this.txtUsername.Focus();
+                    txtUsername.ResetText();
+                    txtPassword.ResetText();
+                }
+            }
+        }
+
+        private void btnSignIn_Click(object sender, EventArgs e)
+        {
+            //btn đăng nhập
+            string username = txtUsername.Text.ToString();
+            string password = txtPassword.Text.ToString();
+            try
+            {
+                //if (loginBL.CheckLogin(username, password))
+                //{
+                //    UserName = username;
+                //    frmProducts frm = new frmProducts();
+                //    this.Hide();
+                //    frm.Show();
+                //}
+            }
+            catch
+            {
+                //Form frm = new frmWP();
+                //frm.ShowDialog();
+                this.btnSignIn.BackgroundImage = null;
+                this.btnSignIn.BackColor = System.Drawing.Color.Black;
+                this.txtUsername.Focus();
+                txtUsername.ResetText();
+                txtPassword.ResetText();
+            }
+
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            //nút thoát frmLogin
+            this.Hide();
+            //Form frm = new frmQuit();//frm thông báo
+            //frm.ShowDialog();
+            this.Show();
+
+        }
+
+        //đổi màu nền btn SignIn
+
+        private void btnSignIn_MouseEnter(object sender, EventArgs e)
+        {
+            // this.btnSignIn.BackgroundImage = ((System.Drawing.Image)
+            //(Project_1.Properties.Resources.Sign_in_bg));
+            this.btnSignIn.BackColor = System.Drawing.Color.MediumBlue;
+
+        }
+
+        private void btnSignIn_MouseLeave(object sender, EventArgs e)
+        {
+            this.btnSignIn.BackColor = System.Drawing.Color.Black;
+        }
+
+        private void btnSignIn_Enter(object sender, EventArgs e)
+        {
+            //this.btnSignIn.BackgroundImage = ((System.Drawing.Image)
+            //(Project_1.Properties.Resources.Sign_in_bg));
+            this.btnSignIn.BackColor = System.Drawing.Color.MediumBlue;
+
+        }
+
+        private void btnSignIn_Leave(object sender, EventArgs e)
+        {
+            this.btnSignIn.BackColor = System.Drawing.Color.Black;
+
+        }
+    }
+}
+
